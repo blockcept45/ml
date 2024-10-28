c5ab582bd604cf1e4ddbd3ca33f38e79288cf3d8
blockcept45 <167816701+blockcept45@users.noreply.github.com>
2024-10-28 23:14:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminPage from './AdminPage';
 import HomePage from './HomePage';
 import Day1 from './Components/ML/Day1';
